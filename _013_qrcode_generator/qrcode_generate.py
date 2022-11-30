a7536beb0d0dcc2950b0f204c748d3ef83ec2520
Christopher Adamson <software.pro.08@gmail.com>
2022-11-29 18:09:39 -0800

    change to use argparse instead of sys.argv

--- _013_qrcode_generator/qrcode_generate.py
+++ _013_qrcode_generator/qrcode_generate.py
@@ -1,15 +1,22 @@
 
 import pyqrcode
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t','--text',help='text to encode in a QR code')
+parser.add_argument('-of','--output_file',help='name of output file w/o extension')
+
+args = parser.parse_args()
 
 # Pull the string to encode from the command line
-string_to_encode = sys.argv[1]
+string_to_encode = args.text
 
 # Generate the QR code
 qrcode = pyqrcode.create(string_to_encode)
 
 # Grab the base filename for the output images
-base_filename = sys.argv[2]
+base_filename = args.output_file
 
 # Output a SVG and PNG
 qrcode.svg(f"{base_filename}.svg", scale=10)
